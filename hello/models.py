8547f1a179a0b169ecd99d159315b0f461baf924
Manish Sagar <adithyaravirao@gmail.com>
2023-03-22 20:21:13 +0530

    commit

--- hello/models.py
+++ hello/models.py
@@ -12,7 +12,7 @@ class Sprofile(models.Model):
     student=models.OneToOneField(User,on_delete=models.CASCADE)
     address=models.CharField(max_length=200)
     phone=models.CharField(max_length=20)
-    image=models.ImageField(default='api.jpg',upload_to='profile_images')
+    image=models.ImageField(upload_to='profile_images')
 
     def __str__(self):
         return f'{self.student.username}-profile'
