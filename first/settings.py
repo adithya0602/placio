89625235bd2eedfa26f7c30ee765e2cb42d27c52
Manish Sagar <adithyaravirao@gmail.com>
2023-05-08 19:55:55 +0530

    commit

--- first/settings.py
+++ first/settings.py
@@ -82,14 +82,10 @@ WSGI_APPLICATION = 'first.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'placio',
-        'USER': 'root',
-        'PASSWORD': '@Achyuth1',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
 
 
