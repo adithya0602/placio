59466621c3cf373eeec9d3aab5175d18aea65f5a
Manish Sagar <adithyaravirao@gmail.com>
2023-05-08 21:30:40 +0530

    commit

--- first/settings.py
+++ first/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^mv&=8%30*_t7!lx_anex+tkiut9r(tv0*mf0-3kiur)tvifv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
