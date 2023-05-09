89b571ab86063f3544f223840e34cc1d498915d4
Manish Sagar <adithyaravirao@gmail.com>
2023-05-09 21:57:11 +0530

    COMMIT

--- first/settings.py
+++ first/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'web-production-f033.up.railway.app'
+    'https://web-production-f033.up.railway.app/'
 ]
 ROOT_URLCONF = 'first.urls'
 
