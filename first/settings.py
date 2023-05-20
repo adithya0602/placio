db55bc77c06ea46c6120215195d53324c25c0682
Manish Sagar <adithyaravirao@gmail.com>
2023-05-20 15:31:09 +0530

    commit

--- first/settings.py
+++ first/settings.py
@@ -95,12 +95,6 @@ DATABASES = {
         'PORT': '6245'
     }
 }
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
 
 
 # Password validation
